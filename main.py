cb6be754f64fa6707f9a216acd22946603012ef2
Hassanibrar632 <hassanibrar632@gmail.com>
2025-02-10 11:14:27 +0500

    Installation of Pakages
    This commit contains the installation an packages and a main.py file for testing.

---
+++ main.py
@@ -0,0 +1,63 @@
+# import Libraries
+import cv2
+import numpy as np
+import face_recognition
+import os
+import matplotlib.pyplot as plt
+import numpy
+
+# set global variables
+path = 'known_faces'
+images = []
+classNames = []
+
+# Fucntion that reads and saves the faces data
+def read_faces(path=path):
+    # Read all images
+    files = os.listdir(path)
+    encodeList = []
+
+    for file in files:
+        classNames.append(os.path.splitext(file)[0])
+        curImg = face_recognition.load_image_file(f'{path}/{file}')
+        # curImg = curImg.astype('uint8')
+
+        encode = face_recognition.face_encodings(curImg)[0]
+        encodeList.append(encode)
+
+    return encodeList
+
+encodeListKnown = read_faces(path)
+print('Encoding Complete')
+
+cap = cv2.VideoCapture(0)
+
+while True:
+    success, img = cap.read()
+    # imgS = cv2.resize(img, (0, 0), None, 0.25, 0.25)
+    imgS = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+
+    facesCurFrame = face_recognition.face_locations(imgS)
+    encodesCurFrame = face_recognition.face_encodings(imgS, facesCurFrame)
+
+    for encodeFace, faceLoc in zip(encodesCurFrame, facesCurFrame):
+        matches = face_recognition.compare_faces(encodeListKnown, encodeFace)
+        faceDis = face_recognition.face_distance(encodeListKnown, encodeFace)
+        matchIndex = np.argmin(faceDis)
+
+        if matches[matchIndex]:
+            name = classNames[matchIndex].upper()
+            y1, x2, y2, x1 = faceLoc
+            y1, x2, y2, x1 = y1 * 4, x2 * 4, y2 * 4, x1 * 4
+            cv2.rectangle(img, (x1, y1), (x2, y2), (0, 255, 0), 2)
+            cv2.rectangle(img, (x1, y2 - 35), (x2, y2), (0, 255, 0), cv2.FILLED)
+            cv2.putText(img, name, (x1 + 6, y2 - 6), cv2.FONT_HERSHEY_COMPLEX, 1, (255, 255, 255), 2)
+        else:
+            y1, x2, y2, x1 = faceLoc
+            y1, x2, y2, x1 = y1 * 4, x2 * 4, y2 * 4, x1 * 4
+            cv2.rectangle(img, (x1, y1), (x2, y2), (255, 0, 0), 2)
+            cv2.rectangle(img, (x1, y2 - 35), (x2, y2), (255, 0, 0), cv2.FILLED)
+            cv2.putText(img, 'unknown', (x1 + 6, y2 - 6), cv2.FONT_HERSHEY_COMPLEX, 1, (255, 255, 255), 2)
+
+    cv2.imshow('Webcam', img)
+    cv2.waitKey(1)
